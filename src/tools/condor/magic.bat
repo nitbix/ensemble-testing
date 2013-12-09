1225f23aeec48511b0d80c435188ac8c31e812ba
Alan Mosca <nitbix@nitbix.com>
2013-12-09 16:46:49 +0000

    new submit files for second experiment set

--- src/tools/condor/magic.bat
+++ src/tools/condor/magic.bat
@@ -1,1 +1,1 @@
-java -cp .;mysql-connector-java-5.1.25-bin.jar main/Test %1 problems\uci_magic 1,3,10,30,100 16000 0.10 10 0.1 rprop mlp:100:sigmoid %2 false 0.3
+java -cp .;mysql-connector-java-5.1.25-bin.jar main/Test %1 problems\uci_magic 1,3,10,30,100 16000 0.10 10 0.1 rprop%4 mlp:100:sigmoid %2 false 0.3 %3
