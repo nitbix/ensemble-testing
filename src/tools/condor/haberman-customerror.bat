5865c5db4548f2d9df667dc2b40ca5c71e5530cf
Alan Mosca <nitbix@nitbix.com>
2013-12-10 11:33:37 +0000

    activationThreshold is constant

--- src/tools/condor/haberman-customerror.bat
+++ src/tools/condor/haberman-customerror.bat
@@ -1,1 +1,1 @@
-java -cp .;mysql-connector-java-5.1.25-bin.jar main/Test %1 problems\uci_haberman 1,3,10,30,100 200 %3 10 %4 rprop%7 mlp:30:sigmoid %2 false %5 %6
+java -cp .;mysql-connector-java-5.1.25-bin.jar main/Test %1 problems\uci_haberman 1,3,10,30,100 200 %3 10 0.1 rprop%6 mlp:30:sigmoid %2 false %4 %5
--- src/tools/condor/letterrecognition-customerror.bat
+++ src/tools/condor/letterrecognition-customerror.bat
@@ -1,1 +1,1 @@
-java -cp .;mysql-connector-java-5.1.25-bin.jar main/Test %1 problems\uci_letterrecognition 1,3,10,30,100 16000 %3 10 %4 rprop%7 mlp:300:sigmoid %2 false %5 %6
+java -cp .;mysql-connector-java-5.1.25-bin.jar main/Test %1 problems\uci_letterrecognition 1,3,10,30,100 16000 %3 10 0.1 rprop%6 mlp:300:sigmoid %2 false %4 %5
