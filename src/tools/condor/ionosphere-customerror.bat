ea879a0b7b27251c1278e0c01a17ef89b8d8a2a8
Alan Mosca <nitbix@nitbix.com>
2013-12-10 10:41:21 +0000

    new jobs for custom error training

---
+++ src/tools/condor/ionosphere-customerror.bat
@@ -0,0 +1,1 @@
+java -cp .;mysql-connector-java-5.1.25-bin.jar main/Test %1 problems\uci_ionosphere 1,3,10,30,100 200 %3 10 %4 rprop%7 mlp:30:sigmoid %2 false %5 %6
---
+++ src/tools/condor/landsat-customerror.bat
@@ -0,0 +1,1 @@
+java -cp .;mysql-connector-java-5.1.25-bin.jar main/Test %1 problems\statlog_landsat 1,3,10,30,100 5000 %3 10 %4 rprop%7 mlp:30:sigmoid %2 false %5 %6
