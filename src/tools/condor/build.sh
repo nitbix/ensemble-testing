f851a980b1969d7b6a003d756607628c91c47fdd
Alan Mosca <nitbix@nitbix.com>
2013-12-11 12:09:27 +0000

    force rebuild of ensemble code

--- src/tools/condor/build.sh
+++ src/tools/condor/build.sh
@@ -4,6 +4,12 @@ git pull -u
 cd ~/git/encog-java-core/src/main/java
 git pull -u
 cd ~/ensemble-testing
+cp ~/git/ensemble-testing/src/tools/condor/*.csub .
+cp ~/git/ensemble-testing/src/tools/condor/*.bat .
+#forced rebuild of all ensemble code
+for target in `find ~/git/encog-java-core/src/main/java/org/encog/ensemble -type d`; do
+	javac -d ~/ensemble-testing -implicit:class -sourcepath ~/git/ensemble-testing/src:~/git/encog-java-core/src/main/java $target/*.java
+done
 javac -d ~/ensemble-testing -implicit:class -sourcepath ~/git/ensemble-testing/src:~/git/encog-java-core/src/main/java ~/git/encog-java-core/src/main/java/org/encog/ml/data/basic/*.java
 javac -d ~/ensemble-testing -implicit:class -sourcepath ~/git/ensemble-testing/src:~/git/encog-java-core/src/main/java ~/git/ensemble-testing/src/helpers/*.java
 javac -d ~/ensemble-testing -implicit:class -sourcepath ~/git/ensemble-testing/src:~/git/encog-java-core/src/main/java ~/git/ensemble-testing/src/main/*.java
