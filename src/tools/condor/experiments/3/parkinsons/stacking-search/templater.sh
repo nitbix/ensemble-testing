fe30a1ece84084b241e98fe5c05d9bbe60b67b54
Alan Mosca <nitbix@nitbix.com>
2014-06-19 22:55:38 +0100

    templates

--- src/tools/condor/experiments/3/parkinsons/stacking-search/templater.sh
+++ src/tools/condor/experiments/3/parkinsons/stacking-search/templater.sh
@@ -2,9 +2,9 @@
 
 cat template.csub > send-all.csub
 
-for te in 0.03 0.01 0.003; do
-for se in 0.03 0.01 0.003; do
-for ste in 0.01 0.001 0.0001 0.00001; do
+for te in 0.01 0.003; do
+for se in 0.05 0.03; do
+for ste in 0.01 0.0001 0.000001; do
 for mid in 10 30 100; do
 	cat template.prop | sed "s/{te}/$te/" | sed "s/{se}/$se/" | sed "s/{ste}/$ste/" | sed "s/{mid}/$mid/" > $te-$se-$ste-$mid.prop
 	echo "arguments = 3 parkinsons stacking-search/$te-$se-$ste-$mid" >> send-all.csub
