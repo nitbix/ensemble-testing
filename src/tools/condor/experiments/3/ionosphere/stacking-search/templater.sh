930b7b7092a970d2cf39f7b40ac627f636a8aa0c
Alan Mosca <nitbix@nitbix.com>
2014-06-19 23:29:25 +0100

    copy/paste error

--- src/tools/condor/experiments/3/ionosphere/stacking-search/templater.sh
+++ src/tools/condor/experiments/3/ionosphere/stacking-search/templater.sh
@@ -7,7 +7,7 @@ for se in 0.3 0.1; do
 for ste in 0.1 0.01 0.001; do
 for mid in 10 30 100; do
 	cat template.prop | sed "s/{te}/$te/" | sed "s/{se}/$se/" | sed "s/{ste}/$ste/" | sed "s/{mid}/$mid/" > $te-$se-$ste-$mid.prop
-	echo "arguments = 3 parkinsons stacking-search/$te-$se-$ste-$mid" >> send-all.csub
+	echo "arguments = 3 ionosphere stacking-search/$te-$se-$ste-$mid" >> send-all.csub
 	echo "queue 30" >> send-all.csub
 done
 done
