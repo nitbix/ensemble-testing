cb87ce23002d0b4331af8783b315be200a701018
Alan Mosca <nitbix@nitbix.com>
2014-06-22 15:09:51 +0100

    missed problem name change

--- src/tools/condor/experiments/3/biodeg/stacking-search/templater.sh
+++ src/tools/condor/experiments/3/biodeg/stacking-search/templater.sh
@@ -8,9 +8,9 @@ for ste in 0.1 0.01 0.001; do
 for mid in 10 30 100; do
 	cat template.prop | sed "s/{te}/$te/" | sed "s/{se}/$se/" | sed "s/{ste}/$ste/" | sed "s/{mid}/$mid/" | sed "s/{extra}//" > $te-$se-$ste-$mid.prop
 	cat template.prop | sed "s/{te}/$te/" | sed "s/{se}/$se/" | sed "s/{ste}/$ste/" | sed "s/{mid}/$mid/" | sed "s/{extra}/-adaptive/" > $te-$se-$ste-$mid-adaptive.prop
-	echo "arguments = 3 ionosphere stacking-search/$te-$se-$ste-$mid" >> send-all.csub
+	echo "arguments = 3 biodeg stacking-search/$te-$se-$ste-$mid" >> send-all.csub
 	echo "queue 30" >> send-all.csub
-	echo "arguments = 3 ionosphere stacking-search/$te-$se-$ste-$mid-adaptive" >> send-all.csub
+	echo "arguments = 3 biodeg stacking-search/$te-$se-$ste-$mid-adaptive" >> send-all.csub
 	echo "queue 30" >> send-all.csub
 done
 done
