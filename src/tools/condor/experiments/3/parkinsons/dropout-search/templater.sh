31eae58b426f6da6cce2fe5e245325460e2bbc05
Alan Mosca <nitbix@nitbix.com>
2014-06-22 15:23:03 +0100

    fix parkinsons dropout file name

--- src/tools/condor/experiments/3/parkinsons/dropout-search/templater.sh
+++ src/tools/condor/experiments/3/parkinsons/dropout-search/templater.sh
@@ -5,7 +5,7 @@ cat template.csub > send-all.csub
 for te in 0.03 0.01 0.001; do
 for se in 0.05 0.03 0.01; do
 for rate in 0.1 0.3 0.5 0.7 0.9; do
-	cat template.prop | sed "s/{te}/$te/" | sed "s/{se}/$se/" | sed "s/{rate}/$rate/" > $te-$se-$ste-$mid.prop
+	cat template.prop | sed "s/{te}/$te/" | sed "s/{se}/$se/" | sed "s/{rate}/$rate/" > $te-$se-$rate.prop
 	echo "arguments = 3 parkinsons dropout-search/$te-$se-$rate" >> send-all.csub
 	echo "queue 30" >> send-all.csub
 done
