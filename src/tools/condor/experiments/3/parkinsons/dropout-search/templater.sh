a48d2e2f439dd40584e5a43719321bf0ab1ca374
Alan Mosca <nitbix@nitbix.com>
2014-06-22 15:22:23 +0100

    fix error changing file name

--- src/tools/condor/experiments/3/parkinsons/dropout-search/templater.sh
+++ src/tools/condor/experiments/3/parkinsons/dropout-search/templater.sh
@@ -6,7 +6,7 @@ for te in 0.03 0.01 0.001; do
 for se in 0.05 0.03 0.01; do
 for rate in 0.1 0.3 0.5 0.7 0.9; do
 	cat template.prop | sed "s/{te}/$te/" | sed "s/{se}/$se/" | sed "s/{rate}/$rate/" > $te-$se-$ste-$mid.prop
-	echo "arguments = 3 parkinsons dropout-search/$te-$se-$rate
+	echo "arguments = 3 parkinsons dropout-search/$te-$se-$rate" >> send-all.csub
 	echo "queue 30" >> send-all.csub
 done
 done
