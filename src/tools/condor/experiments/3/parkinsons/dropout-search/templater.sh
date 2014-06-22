8f8257dc661eca7c52f8d0bb8411871211e6c190
Alan Mosca <nitbix@nitbix.com>
2014-06-22 15:19:59 +0100

    change dropout search params

--- src/tools/condor/experiments/3/parkinsons/dropout-search/templater.sh
+++ src/tools/condor/experiments/3/parkinsons/dropout-search/templater.sh
@@ -2,7 +2,7 @@
 
 cat template.csub > send-all.csub
 
-for te in 0.03 0.01 0.003; do
+for te in 0.03 0.01 0.001; do
 for se in 0.05 0.03 0.01; do
 for rate in 0.1 0.3 0.5 0.7 0.9; do
 	cat template.prop | sed "s/{te}/$te/" | sed "s/{se}/$se/" | sed "s/{rate}/$rate/" > $te-$se-$ste-$mid.prop
