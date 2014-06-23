04451e1233e5fa1223bfa6164d64367b96c37abc
Alan Mosca <nitbix@nitbix.com>
2014-06-23 15:07:42 +0100

    mistake in templater - wrong technique

--- src/tools/condor/experiments/3/biodeg/adaboost-search/templater.sh
+++ src/tools/condor/experiments/3/biodeg/adaboost-search/templater.sh
@@ -5,7 +5,7 @@ cat template.csub > send-all.csub
 for te in 0.1 0.03 0.01; do
 for se in 0.15 0.1 0.03; do
 	cat template.prop | sed "s/{te}/$te/" | sed "s/{se}/$se/" | sed "s/{rate}/$rate/" > $te-$se.prop
-	echo "arguments = 3 biodeg dropout-search/$te-$se" >> send-all.csub
+	echo "arguments = 3 biodeg adaboost-search/$te-$se" >> send-all.csub
 	echo "queue 30" >> send-all.csub
 done
 done
