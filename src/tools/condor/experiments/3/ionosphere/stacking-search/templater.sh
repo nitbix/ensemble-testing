c77560c6fa25567c3b182342125803794045ac3d
Alan Mosca <nitbix@nitbix.com>
2015-03-01 20:06:14 +0000

    cleanup

--- src/tools/condor/experiments/3/ionosphere/stacking-search/templater.sh
+++ src/tools/condor/experiments/3/ionosphere/stacking-search/templater.sh
@@ -2,13 +2,13 @@
 
 cat template.csub > send-all.csub
 
-for te in 0.3 0.1; do
-for se in 0.3 0.1; do
-for ste in 0.1 0.01 0.001; do
+for te in 0.1; do
+for se in 0.3; do
+for ste in 0.01 0.001; do
 for mid in 10 30 100; do
 	cat template.prop | sed "s/{te}/$te/" | sed "s/{se}/$se/" | sed "s/{ste}/$ste/" | sed "s/{mid}/$mid/" > $te-$se-$ste-$mid.prop
 	echo "arguments = 3 ionosphere stacking-search/$te-$se-$ste-$mid" >> send-all.csub
-	echo "queue 30" >> send-all.csub
+	echo "queue 100" >> send-all.csub
 done
 done
 done
