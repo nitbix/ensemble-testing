681aa955ccf654645981e49897a5bd3ee97b2439
Alan Mosca <nitbix@nitbix.com>
2015-06-05 11:35:48 +0100

    add transform

---
+++ src/dropout_training_experiment/transform.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+import data
+import sys
+import cPickle
+import gzip
+import os
+import numpy as np
+
+if __name__ == '__main__':
+    if len(sys.argv) > 2:
+        dataset = sys.argv[1]
+        fileName = sys.argv[2]
+    else:
+        dataset='mnist.pkl.gz'
+        fileName='mnist-transformed'
+    dataset = data.load_data(dataset,shared=False)
+    train,valid,test = dataset
+    train_x, train_y = train
+    valid_x, valid_y = valid
+    test_x, test_y = test
+    aggregate_x = np.concatenate((train_x, valid_x), axis=0)
+    aggregate_y = np.concatenate((train_y, valid_y), axis=0)
+    t = data.Transformer((aggregate_x,aggregate_y),28,28,progress=True)
+    aggregate_train = t.get_data()
+    aggregate_valid = (aggregate_x, aggregate_y)
+    atx,aty = aggregate_train
+    transformed_dataset = (aggregate_train,aggregate_valid,test)
+    print "... saving"
+    f = open(fileName + '.pkl','wb')
+    cPickle.dump(transformed_dataset,f)
+    f.close()
