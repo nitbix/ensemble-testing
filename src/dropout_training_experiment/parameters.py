c0a176c8aa4885daaf97f065dd7ca43b151f0485
nitbix <nitbix@nitbix.com>
2015-08-26 00:13:52 +0100

    started working on yaml parameter representations

---
+++ src/dropout_training_experiment/parameters.py
@@ -0,0 +1,33 @@
+import yaml 
+import update_rules
+
+class Parameters(object):
+
+#	def __init__(self):
+#        self.dataset = 'mnist'
+#        self.L1_reg = 0
+#        self.L2_reg = 0
+#        self.n_epochs = 100
+#        self.transform = False
+#        self.learning_rate = 0.001
+#        self.pretraining = None
+#        self.pretraining_passes = 1
+#        self.training_method = 'normal'
+#        self.update_rule = update_rules.sgd
+#        self.batch_size = 300
+#        self.n_hidden = [
+#                  ('flat',(2500,0.5,'h0',T.tanh)),
+#                  ('flat',(2000,0.5,'h1',T.tanh)),
+#                  ('flat',(1500,0.5,'h2',T.tanh)),
+#                  ('flat',(1000,0.5,'h2',T.tanh)),
+#                  ('flat',(500,0.5,'h3',T.tanh))
+#                 ]
+#        n_in = 784
+
+    def __init__(self, **entries): 
+        self.__dict__.update(entries)
+
+def load_parameters(filename):
+    with open(filename) as f:
+        r = yaml.safe_load(f)
+    return Parameters(r)
