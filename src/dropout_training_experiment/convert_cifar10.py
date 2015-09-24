279ee8dc98d41a1c420a11c9f73d2044f8add35e
nitbix <nitbix@nitbix.com>
2015-09-25 00:19:07 +0100

    change to specified dir

--- src/dropout_training_experiment/convert_cifar10.py
+++ src/dropout_training_experiment/convert_cifar10.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+import os
+import sys
 import cPickle
 import numpy as np
 
@@ -12,11 +14,14 @@ def unpickle(file):
     return dict
 
 if __name__ == '__main__':
+    if len(sys.argv) >= 2:
+        os.chdir(sys.argv[1])
     data = [ unpickle('cifar-10-batches-py/data_batch_' + str(x)) for x in xrange(1,6) ]
     test = unpickle('cifar-10-batches-py/test_batch')
 
-    merged_train_x = np.concatenate([x['data'] for x in data[0:3]], axis = 0)
-    merged_train_y = np.concatenate([x['labels'] for x in data[0:3]], axis = 0)
+    merged_train_x = np.concatenate([x['data'] for x in data[0:4]], axis = 0)
+    merged_train_y = np.concatenate([x['labels'] for x in data[0:4]], axis = 0)
+    print len(merged_train_x)
     valid_x = data[4]['data']
     valid_y = data[4]['labels']
     test_x = test['data']
