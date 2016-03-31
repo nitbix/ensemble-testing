f9cc6d2d52d80c192b4400d3323f033f2f81b7f0
Alan Mosca <nitbix@nitbix.com>
2016-03-31 23:26:30 +0200

    stub latex table

--- src/dropout_training_experiment/uprop_stats.py
+++ src/dropout_training_experiment/uprop_stats.py
@@ -36,15 +36,35 @@ pipeline = [
 ]
 
 cursor = db.uprop_paper.aggregate(pipeline=pipeline)
-
+means  = {}
+stdevs = {}
+methods = []
+datasets = []
 for r in cursor['result']:
-    print "dataset: {0}".format(r['_id']['params_dataset'])
-    print "arch: {0}".format(r['_id']['params_n_hidden'][0][0])
-    print "update_rule: {0}".format(r['_id']['params_update_rule'])
-    print "transform: {0}".format(r['_id']['params_online_transform'])
+    x = r['_id']
+    print "dataset: {0}".format(x['params_dataset'])
+    print "arch: {0}".format(x['params_n_hidden'][0][0])
+    print "update_rule: {0}".format(x['params_update_rule'])
+    print "transform: {0}".format(x['params_online_transform'])
     print "  count: {0}".format(r['count'])
     print "  avg_best_epoch: {0}".format(r['avg_best_epoch'])
     print "  avg_best_valid: {0}".format(r['avg_best_valid'])
     print "  avg_best_test: {0}".format(r['avg_best_test'])
     #print r
     print "-----------"
+    dataset = "{0}-{1}".format(x['params_dataset'], x['params_online_transform'])
+    if dataset not in datasets:
+        datasets.append(dataset)
+    if method not in methods:
+        methods.append(method)
+    if dataset not in means:
+        means[dataset] = {}
+    if mehtod not in means[dataset]:
+        means[dataset][method] = {}
+
+for dataset in datasets:
+    print " & " + " & ".join(methods)
+    line = []
+    for method in methods:
+        line.append(means[dataset][method])
+    print dataset + " & " + " & ".join(line)
