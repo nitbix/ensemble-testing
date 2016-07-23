f9193f65368b2701e42ff29d2f4eb9361e9306a4
Alan Mosca <nitbix@nitbix.com>
2016-07-23 22:05:37 +0100

    make error bars

--- src/dropout_training_experiment/make_side_by_side_graphs.sh
+++ src/dropout_training_experiment/make_side_by_side_graphs.sh
@@ -14,8 +14,8 @@ dumped_dir=/var/www/html/dumped/graphs
 echo "
 #set style line 1 lc rgb '#8b1a0e' lw 2 # --- red
 #set style line 2 lc rgb '#5e9c36' lw 2 # --- green
-set style line 1 lc rgb '#993300' lw 2 # --- orange
-set style line 2 lc rgb '#003366' lw 2 # --- blue
+set style line 1 lc rgb '#339933' lw 2 # --- green
+set style line 2 lc rgb '#0033AA' lw 2 # --- blue
 set style line 11 lc rgb '#808080' lt 1
 set border 3 back ls 11
 set tics nomirror
@@ -27,8 +27,11 @@ set xlabel \"Training Epoch\"
 set ylabel \"${error_name} Error (%)\"
 set terminal postscript eps enhanced color size 4,2.5
 set out \"${out_file}.eps\"
-plot \"${first_file}\"  every ::0::${epochs} using 1:2 lw 2 title \"${first_label}\" with lines,\
-     \"${second_file}\" every ::0::${epochs} using 1:2 lw 2 title \"${second_label}\" with lines
+plot \"${first_file}\"  every ::0::${epochs} using 1:2 linestyle 1 title \"${first_label}\" with lines,\
+     \"${second_file}\" every ::0::${epochs} using 1:2 linestyle 2 title \"${second_label}\" with lines,\
+     \"${first_file}\" every ::0::${epochs} notitle linestyle 1 with errorbars,\
+     \"${second_file}\" every ::0::${epochs} notitle linestyle 2 with errorbars
+
 set terminal png
 set out \"${out_file}.png\"
 replot
